0f1da1bfe8ebb3d09a32979ac99b9d8ece0e1f4c
rafaeldefault <rafaeldefault@gmail.com>
2025-03-11 00:16:31 -0300

    Criação do botão de enviar e animação de send

--- MVC_Chameleon/wwwroot/js/site.js
+++ MVC_Chameleon/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).jquery(function () {
+    $(".btn-enviar").on("click", function () {
+        let $btn = $(this);
+        let $icon = $btn.find(".icon-enviar");
+        let $text = $btn.find(".text");
+
+        // Faz o ícone desaparecer para a direita e o texto voltar ao centro
+        $icon.css({ right: "-30px", opacity: 0 });
+        $text.css({ transform: "translateX(0px)" });
+
+        // Delay para reiniciar animação
+        setTimeout(() => {
+            $icon.css({ right: "10px", opacity: 1 });
+            $text.css({ transform: "translateX(-10px)" });
+        }, 500);
+    });
+});
